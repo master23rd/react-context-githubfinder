4db88838769910a81cd0c98164886fe9c45a5680
Adam A Prasetya <adamworks23@gmail.com>
2022-12-30 14:33:49 +0700

    base code react-tailwind

---
+++ src/components/users/UserItem.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+function UserItem({ user: { login, avatar_url } }) {
+  //direct print
+  //   return <div>{user.login}</div>
+
+  //using deconstruct object login
+  return (
+    <div className='card shadow-md compact side bg-base-100'>
+      <div className='flex-row items-center space-x-4 card-body'>
+        <div>
+          <div className='avatar'>
+            <div className='rounded-full shadow w-14 h-14'>
+              <img src={avatar_url} alt='profile' />
+            </div>
+          </div>
+        </div>
+        <div>
+          <h2 className='card-title'>{login}</h2>
+          <Link casslName='text-bae-content text opacity' to={'/users'}>
+            visit profile
+          </Link>
+        </div>
+      </div>{' '}
+    </div>
+  )
+}
+
+UserItem.prototype = {
+  user: PropTypes.object.isRequired,
+}
+
+export default UserItem
